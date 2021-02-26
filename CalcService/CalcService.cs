c88313cd96b28a3a11962f8c6006cee10b54ac36
zkdev <dev@zeekay.dev>
2021-02-26 15:25:40 +0100

    Use whitespace as indent

--- CalcService/CalcService.cs
+++ CalcService/CalcService.cs
@@ -1,13 +1,13 @@
 ﻿namespace Service
 {
-	public class CalcService : ICalcService
-	{
-		public Result Add(Arguments args)
-		{
-			return new Result
-			{
-				Value = args.Arg1 + args.Arg2
-			};
-		}
-	}
+    public class CalcService : ICalcService
+    {
+        public Result Add(Arguments args)
+        {
+            return new Result
+            {
+                Value = args.Arg1 + args.Arg2
+            };
+        }
+    }
 }
